51e4731d60f444c0b5f7dafef9a77f450591df58
SusannahHolmes <susannahrholmes@gmail.com>
2022-08-01 16:36:17 +0800

    h5 integration, 1D plots and updated yml
    	renamed:    dragonfly_conda.yml -> dragonfly_py2.yml
    	new file:   dragonfly_py3.yml for a python3 environment
    	new file:   utils/1Dplot.py for plotting 1D plots from powder
    	modified:   utils/powder.py now with h5 integration

---
+++ utils/1Dplot.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+### must run powder_h5.py first in order to get the powder output that is used as input in this python script
+
+
+'''Module to calculate and save powder sum of frames'''
+
+from __future__ import print_function
+import numpy as np
+import h5py
+from matplotlib import pyplot as plt
+from py_src import py_utils
+from py_src import read_config
+from py_src import detector
+
+def main():
+    # Read detector and photons file from config
+    parser = py_utils.MyArgparser()
+    args = parser.special_parse_args()
+
+    #location of powder
+    try:
+        det_fname = read_config.get_filename(args.config_file, 'emc', 'in_detector_file')
+    except:
+        print('emc:::in_detector_file not found. Note that single detector file needed for powder sum')
+
+    det = detector.Detector(det_fname)
+    dataloc = 'data/powder_%d_%d.csv'%det.frame_shape
+    data_array = np.genfromtxt(dataloc, delimiter=',')
+
+    ##visualise powder image
+    #plt.imshow(data_array, interpolation='nearest')
+    #plt.show()
+
+    #Get 1D plot
+    try:
+        detd = read_config.get_filename(args.config_file, 'parameters', 'detd') #in mm
+        pixsize = read_config.get_filename(args.config_file, 'parameters', 'pixsize') #in mm
+        lambd = read_config.get_filename(args.config_file, 'parameters', 'lambda') #in angstrom
+        stoprad = read_config.get_filename(args.config_file, 'parameters', 'stoprad') #in pixels
+    except:
+        print('parameters not found in config.ini file')
+        return
+    
+    center = data_array.shape[0]/2 #assuming square detector
+    y,x=np.indices((data_array.shape))
+    r = np.sqrt((x-center)**2+(y-center)**2)
+    r = r.astype(np.int)
+    tbin = np.bincount(r.ravel(), data_array.ravel())
+    nr = np.bincount(r.ravel())
+    radialprofile = tbin / nr
+    xrad_pix = range(len(nr))
+    gamma = [np.arctan((i*float(pixsize))/float(detd)) for i in xrad_pix] #in degrees
+    d = [float(lambd)/(2*np.sin(i/2)) for i in gamma] #in angstrom
+    plt.plot(d,radialprofile)
+    plt.xlim([d[int(stoprad)+1],d[-1]])
+    plt.show()
+
+if __name__ == "__main__":
+    main()
--- utils/powder.py
+++ utils/powder.py
@@ -4,6 +4,8 @@
 
 from __future__ import print_function
 import numpy as np
+import h5py
+from matplotlib import pyplot as plt
 from py_src import py_utils
 from py_src import read_config
 from py_src import detector
@@ -13,48 +15,65 @@ def main():
     # Read detector and photons file from config
     parser = py_utils.MyArgparser(description="Generate virtual powder pattern")
     args = parser.special_parse_args()
-
+    
     try:
         photons_list = [read_config.get_filename(args.config_file, 'emc', "in_photons_file")]
     except read_config.configparser.NoOptionError:
         with open(read_config.get_param(args.config_file, 'emc', "in_photons_list"), 'r') as fptr:
             photons_list = [line.rstrip() for line in fptr.readlines()]
-
     try:
         det_fname = read_config.get_filename(args.config_file, 'emc', 'in_detector_file')
     except:
         print('emc:::in_detector_file not found. Note that single detector file needed for powder sum')
         return
+    
     det = detector.Detector(det_fname)
-
-    powder = np.zeros(det.x.shape)
-    assem_powder = np.zeros(det.frame_shape)
-
-    # For each emc file, read data and add to powder
-    for photons_file in photons_list:
-        # Read photon data
-        with open(photons_file, 'rb') as fptr:
-            num_data = np.fromfile(fptr, dtype='i4', count=1)[0]
-            num_pix = np.fromfile(fptr, dtype='i4', count=1)[0]
-            print(photons_file+ ': num_data = %d, num_pix = %d' % (num_data, num_pix))
-            if num_pix != len(powder):
-                print('Detector and photons file dont agree on num_pix')
-            fptr.seek(1024, 0)
-            ones = np.fromfile(fptr, dtype='i4', count=num_data)
-            multi = np.fromfile(fptr, dtype='i4', count=num_data)
-            place_ones = np.fromfile(fptr, dtype='i4', count=ones.sum())
-            place_multi = np.fromfile(fptr, dtype='i4', count=multi.sum())
-            count_multi = np.fromfile(fptr, dtype='i4', count=multi.sum())
-
-        # Place photons in powder array
-        np.add.at(powder, place_ones, 1)
-        np.add.at(powder, place_multi, count_multi)
-        np.add.at(assem_powder, (det.x[place_ones], det.y[place_ones]), 1)
-        np.add.at(assem_powder, (det.x[place_multi], det.y[place_multi]), count_multi)
-
+    powder = np.zeros(det.x.shape, dtype=np.int32)
+    assem_powder = np.zeros(det.frame_shape, dtype=np.int32)
+    
+    #if photons input file is a .emc extension - is this right? or is it .bin? the wiki said the sparse data format is .emc
+    if str(photons_list[0]).split('.')[-1] == 'emc':
+        # For each emc file, read data and add to powder
+        for photons_file in photons_list:
+            # Read photon data
+            with open(photons_file, 'rb') as fptr:
+                num_data = np.fromfile(fptr, dtype='i4', count=1)[0]
+                num_pix = np.fromfile(fptr, dtype='i4', count=1)[0]
+                print(photons_file+ ': num_data = %d, num_pix = %d' % (num_data, num_pix))
+                if num_pix != len(powder):
+                    print('Detector and photons file dont agree on num_pix')
+                fptr.seek(1024, 0)
+                ones = np.fromfile(fptr, dtype='i4', count=num_data)
+                multi = np.fromfile(fptr, dtype='i4', count=num_data)
+                place_ones = np.fromfile(fptr, dtype='i4', count=ones.sum())
+                place_multi = np.fromfile(fptr, dtype='i4', count=multi.sum())
+                count_multi = np.fromfile(fptr, dtype='i4', count=multi.sum())
+    
+    #if photons input file is .h5 extension
+    elif str(photons_list[0]).split('.')[-1] == 'h5':
+        # For each emc file, read data and add to powder
+        for photons_file in photons_list:
+            # Read photon data
+            with h5py.File(photons_file) as fptr:
+                num_data = fptr['count_multi'].shape[0]
+                num_pix = fptr['num_pix'][0]
+                print(photons_file+ ': num_data = %d, num_pix = %d' % (num_data, num_pix))
+                if num_pix != len(powder):
+                    print('Detector and photons file dont agree on num_pix')
+                place_ones = np.hstack(np.array(fptr['place_ones']))
+                place_multi = np.hstack(np.array(fptr['place_multi']))
+                count_multi = np.hstack(np.array(fptr['count_multi']))
+    else:
+        print('photons file type not supported')
+            
+    # Place photons in powder array
+    np.add.at(powder, place_ones, 1)
+    np.add.at(powder, place_multi, count_multi)
+    np.add.at(assem_powder, (det.x[place_ones], det.y[place_ones]), 1)
+    np.add.at(assem_powder, (det.x[place_multi], det.y[place_multi]), count_multi)
     # Write float64 array to file
     powder.tofile('data/powder.bin')
-    assem_powder.tofile('data/powder_%d_%d.bin'%det.frame_shape)
+    np.savetxt('data/powder_%d_%d.csv'%det.frame_shape, assem_powder,delimiter=',')
 
 if __name__ == "__main__":
     main()
